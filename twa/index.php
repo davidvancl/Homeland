d9cf3c0a278dd7c9b8b8c8591de1aa4027d3de34
David_Vancl <david.vancl@tul.cz>
2021-03-03 11:12:46 +0100

    init repos

---
+++ twa/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="cs">
+    <head>
+        <title>Grand Theft Auto V</title>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="content-type" content="text/html">
+        <meta name="author" content="David Vancl">
+        <meta http-equiv="Content-language" content="cs">
+        <meta name="keywords" content="GTA, GTA 5, GTA V,grand theft auto, grand theft auto 5, grand theft auto V">
+        <meta name="description" content="Stránka s informacemi a historií hry GTA 5.">.
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="icon" href="Images/icon.ico" type="image/ico" sizes="16x16">
+        <link rel="stylesheet" href="Styles/main_style.css">
+    </head>
+    <body>
+        <header id="header">
+            <img id="headerLogo" src="Images/header_logo.png" alt="Grand Theft Auto V">
+            <h1 id="headerTitle" class="GTAFont">Grand Theft Auto</h1>
+            <nav>
+                <hgroup>
+                    <a href="#"><h2 class="GTAFont navItem">Historie</h2></a>
+                    <a href="#"><h2 class="GTAFont navItem">Informace</h2></a>
+                    <a href="#"><h2 class="GTAFont navItem">Recenze</h2></a>
+                    <a href="#"><h2 class="GTAFont navItem">Kontakt</h2></a>
+                </hgroup>
+            </nav>
+        </header>
+
+        <section id="slider">
+            <figure>
+                <img src="Images/Slider/1.jpg" alt="">
+                <img src="Images/Slider/2.jpg" alt="">
+                <img src="Images/Slider/3.jpg" alt="">
+                <img src="Images/Slider/4.jpg" alt="">
+                <img src="Images/Slider/1.jpg" alt="">
+            </figure>
+            <img id="sliderLogo" src="Images/Slider/LogoSlider.svg" alt="">
+        </section>
+        
+<!--        <main>-->
+<!--            <article> Vlastní text článku.-->
+<!--                    <section>První kapitola, třeba i s podnadpisy</section>-->
+<!--                    <section>Druhá kapitola nebo jakkoli odlišný obsah</section>-->
+<!--            <aside>Sloupeček, který ale ještě patří do článku</aside>-->
+<!--        </main>-->
+<!--        <nav>spodní navigace</nav>-->
+<!--        <aside>reklama</aside>-->
+<!--        <footer>patička</footer>-->
+    <br>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </body>
+</html>
