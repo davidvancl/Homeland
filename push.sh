7c08740cd9cb41b1bbd7ae4da4965c0a760c7c6d
David Vancl <vancldada@gmail.com>
2021-03-03 12:43:15 +0000

    Version 1.02

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ cd /var/www/html/dave/
 git add .
 version=`cat /var/www/html/dave/twa_version.conf`
 version=$(echo "scale=2; $version+0.01" | bc)
-git commit -m `echo "$version"`
+git commit -m "Version `echo "$version"`"
 git push
