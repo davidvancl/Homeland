c9acc8a5caedfb1eb3aa548e8ae71923ca2484e9
davidvancl <vancldada@gmail.com>
2021-03-08 15:18:07 +0000

    Version 1.28

--- twa/Scripts/menu_script.js
+++ twa/Scripts/menu_script.js
@@ -1,6 +1,6 @@
 function changeMenuVisibility(){
     if(document.getElementById("sub_nav").style.height === "0px"){
-        document.getElementById("sub_nav").style.height = "270px";
+        document.getElementById("sub_nav").style.height = "240px";
     } else {
         document.getElementById("sub_nav").style.height = "0px";
     }
