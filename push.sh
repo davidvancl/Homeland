a32523c110d7b30cb366145aa764416736a92592
davidvancl <vancldada@gmail.com>
2021-03-16 09:28:50 +0000

    clear repos

--- push.sh
+++
@@ -1,7 +0,0 @@
-cd /var/www/html/dave/
-git add .
-version=`cat /var/www/html/dave/twa_version.conf`
-version=$(echo "scale=2; $version+0.01" | bc)
-git commit -m "Version `echo "$version"`"
-git push
-echo $version > /var/www/html/dave/twa_version.conf
