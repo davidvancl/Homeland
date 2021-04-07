ab50549ca476b09d4d0d20d0ed2ab40de918f076
davidvancl <vancldada@gmail.com>
2021-04-07 10:41:58 +0000

    Version 1.56

--- mopa_arduino/_ATMega2560/_ATMega2560.ino
+++ mopa_arduino/_ATMega2560/_ATMega2560.ino
@@ -4,6 +4,7 @@
  Author:  David Vancl
 */
 #include "DHT.h"
+#include "RTClib.h"
 
 #define DHT_PIN_INSIDE   2 
 #define DHT_PIN_OUTSIDE  3 
@@ -11,18 +12,25 @@
 #define SERIAL_SPEED     9600
 #define DATABASE         "Mongo"
 #define RSA_KEY          "test"
+#define MEASURE_DELAY    10000
+#define CO2_DELAY        30000
+#define LED_DELAY        500
+#define RED_LED          8
+#define GREEN_LED        9
 
 byte getCO2cmd[] = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
-char co2_value_inside[9];
-char co2_value_outside[9];
+char co2_value_inside[9], co2_value_outside[9];
+
+String serial_string, last_inside_co2, last_outside_co2;
+unsigned long send_millis, co2_millis;
 
 DHT dht_inside_sensor(DHT_PIN_INSIDE, DHT_TYPE);
 DHT dht_outside_sensor(DHT_PIN_OUTSIDE, DHT_TYPE);
-
-String serial_string;
-unsigned long send_millis;
+RTC_Millis rtc;
 
 void setup() {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
   Serial.begin(SERIAL_SPEED);
   Serial1.begin(SERIAL_SPEED);
   Serial2.begin(SERIAL_SPEED);
@@ -30,32 +38,36 @@ void setup() {
   dht_inside_sensor.begin();
   dht_outside_sensor.begin();
   send_millis = millis();
+  co2_millis = millis();
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+
+  digitalWrite(RED_LED, HIGH);
+  delay(180000);
+  digitalWrite(RED_LED, LOW);
 }
 
 void loop() {
-  if (millis() - send_millis >= 30000) {
-    
+  if (millis() - send_millis >= MEASURE_DELAY) {
     float inside_humidity = dht_inside_sensor.readHumidity();
     float inside_temperature = dht_inside_sensor.readTemperature();
-    
     float outside_humidity = dht_outside_sensor.readHumidity();
     float outside_temperature = dht_outside_sensor.readTemperature();
-
-    Serial2.write(getCO2cmd, 9);
-    Serial2.readBytes(co2_value_inside, 9);
-    int inside_high = (int) co2_value_inside[2];
-    int inside_low  = (int) co2_value_inside[3];
-    int inside_pulse = (256 * inside_high) + inside_low;
-    String inside_co2 = String(inside_pulse);
-
-    Serial1.write(getCO2cmd, 9);
-    Serial1.readBytes(co2_value_outside, 9);
-    int outside_high = (int) co2_value_outside[2];
-    int outside_low  = (int) co2_value_outside[3];
-    int outside_pulse = (256 * outside_high) + outside_low;
-    String outside_co2 = String(outside_pulse);
+    String outside_co2 = "";
+    String inside_co2 = "";
     
-    String date_time = "2021-03-29 12:47:43";
+    if (millis() - co2_millis >= CO2_DELAY) {
+      outside_co2 = getOutsideCO2();
+      inside_co2 = getInsideCO2();
+      last_inside_co2 = inside_co2;
+      last_outside_co2 = outside_co2;
+      co2_millis = millis();
+    } else {
+      outside_co2 = last_outside_co2;
+      inside_co2 = last_inside_co2;
+    }
+   
+    String date_time = getNow();
+    
     if (isnan(inside_humidity) || isnan(inside_temperature) || isnan(outside_humidity) || isnan(outside_temperature)) {
       return String("\"error_code\":\"404\"");
     }
@@ -85,12 +97,43 @@ void loop() {
   }
 }
 
+String getNow(){
+  DateTime actual = rtc.now();
+  char dateBuffer[12];
+  char timeBuffer[12];
+  sprintf(dateBuffer,"%04u-%02u-%02u ", actual.year(), actual.day(),actual.month());
+  sprintf(timeBuffer,"%02u:%02u:%02u", actual.hour(), actual.minute(), actual.second());
+
+  return String(dateBuffer) + "" + (timeBuffer);
+}
+
+String getOutsideCO2(){
+    Serial1.write(getCO2cmd, 9);
+    Serial1.readBytes(co2_value_outside, 9);
+    int outside_high = (int) co2_value_outside[2];
+    int outside_low  = (int) co2_value_outside[3];
+    int outside_pulse = (256 * outside_high) + outside_low;
+    return String(outside_pulse);
+}
+
+String getInsideCO2(){
+    Serial2.write(getCO2cmd, 9);
+    Serial2.readBytes(co2_value_inside, 9);
+    int inside_high = (int) co2_value_inside[2];
+    int inside_low  = (int) co2_value_inside[3];
+    int inside_pulse = (256 * inside_high) + inside_low;
+    return String(inside_pulse);
+}
+
 void serialEvent3() {
   while (Serial3.available()) {
     char serial_char = Serial3.read();
     serial_string += serial_char;
     if (serial_char == '}') {
       Serial.println(serial_string);
+      if (serial_string == "{OK}"){
+        
+      } 
       serial_string = "";
     }
   }
