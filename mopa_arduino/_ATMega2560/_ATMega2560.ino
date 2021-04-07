de223f96e6e69d18a0b06147075c008fa2535fe0
davidvancl <vancldada@gmail.com>
2021-04-07 08:49:29 +0000

    Version 1.55

--- mopa_arduino/_ATMega2560/_ATMega2560.ino
+++ mopa_arduino/_ATMega2560/_ATMega2560.ino
@@ -12,6 +12,10 @@
 #define DATABASE         "Mongo"
 #define RSA_KEY          "test"
 
+byte getCO2cmd[] = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
+char co2_value_inside[9];
+char co2_value_outside[9];
+
 DHT dht_inside_sensor(DHT_PIN_INSIDE, DHT_TYPE);
 DHT dht_outside_sensor(DHT_PIN_OUTSIDE, DHT_TYPE);
 
@@ -20,6 +24,8 @@ unsigned long send_millis;
 
 void setup() {
   Serial.begin(SERIAL_SPEED);
+  Serial1.begin(SERIAL_SPEED);
+  Serial2.begin(SERIAL_SPEED);
   Serial3.begin(SERIAL_SPEED);
   dht_inside_sensor.begin();
   dht_outside_sensor.begin();
@@ -27,17 +33,31 @@ void setup() {
 }
 
 void loop() {
-  if (millis() - send_millis >= 10000) {
- 
+  if (millis() - send_millis >= 30000) {
+    
     float inside_humidity = dht_inside_sensor.readHumidity();
     float inside_temperature = dht_inside_sensor.readTemperature();
+    
     float outside_humidity = dht_outside_sensor.readHumidity();
     float outside_temperature = dht_outside_sensor.readTemperature();
-    float inside_co2 = 5.0;
-    float outside_co2 = 5.0;
+
+    Serial2.write(getCO2cmd, 9);
+    Serial2.readBytes(co2_value_inside, 9);
+    int inside_high = (int) co2_value_inside[2];
+    int inside_low  = (int) co2_value_inside[3];
+    int inside_pulse = (256 * inside_high) + inside_low;
+    String inside_co2 = String(inside_pulse);
+
+    Serial1.write(getCO2cmd, 9);
+    Serial1.readBytes(co2_value_outside, 9);
+    int outside_high = (int) co2_value_outside[2];
+    int outside_low  = (int) co2_value_outside[3];
+    int outside_pulse = (256 * outside_high) + outside_low;
+    String outside_co2 = String(outside_pulse);
+    
     String date_time = "2021-03-29 12:47:43";
     if (isnan(inside_humidity) || isnan(inside_temperature) || isnan(outside_humidity) || isnan(outside_temperature)) {
-      /*return String("\"error_code\":\"404\"");*/
+      return String("\"error_code\":\"404\"");
     }
   
     Serial3.print(F("{device_id=arduino_station"));
@@ -60,7 +80,6 @@ void loop() {
     Serial3.print(F("&rsa_key="));
     Serial3.print(String(RSA_KEY));
     Serial3.print(F("}"));
-
 
     send_millis = millis();
   }
